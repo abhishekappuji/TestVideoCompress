95f2f0c39c03fdca7101d2137bfe2fe672008446
abhishekappuji <abhishekkumartiwari@hotmail.com>
2020-07-19 02:00:42 -0700

    Initial Commit

--- src/main/java/com/example/compressvideo/VideoActivity.kt
+++ src/main/java/com/example/compressvideo/VideoActivity.kt
@@ -5,12 +5,14 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.MediaController
+import android.widget.TextView
 import android.widget.VideoView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_video.*
 
 class VideoActivity : AppCompatActivity() {
     var simpleVideoView: VideoView? = null
+    var textView: TextView? = null
     var mediaControls: MediaController? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +25,18 @@ class VideoActivity : AppCompatActivity() {
         //abhi.text=abc.toString()
         //val xyz:Uri= Uri.parse(abcd)
         simpleVideoView=findViewById(R.id.play) as VideoView
+        textView=findViewById(R.id.compress) as TextView
         mediaControls = MediaController(this)
         mediaControls!!.setAnchorView(this.simpleVideoView)
         simpleVideoView!!.setMediaController(mediaControls)
-        abhi.text=intent.getStringExtra("path")
+        //abhi.text=intent.getStringExtra("path")
       play.setVideoURI(Uri.parse(intent.getStringExtra("path")))
+
+        compress.setOnClickListener(){
+            compressVideo()
+        }
+    }
+    fun compressVideo(){
+
     }
 }
